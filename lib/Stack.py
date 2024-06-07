8d95b7e970fba8f52649d31c38fd3e6c2313bf49
Stephen Muriithi Munene <smuriithi7969@gmail.com>
2024-06-07 18:58:01 +0300

    submits stack lab

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,30 @@
 class Stack:
 
-    def __init__(self, items = [], limit = 100):
-        pass
+    def __init__(self, items = None, limit = 100):
+        # self.items = items
+        self.items = items if items is not None else []
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0 
 
     def push(self, item):
-        pass
+        return self.items.append(item)
 
     def pop(self):
-        pass
+        return self.items.pop()
 
     def peek(self):
-        pass
+        return self.items[-1]
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return len(self.items) == self.limit
 
     def search(self, target):
-        pass
+        try:
+            return self.items[target]
+        except ValueError:
+            return -1
